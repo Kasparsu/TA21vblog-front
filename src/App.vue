320d4cad72091eacd3191879f01ba660fb92db4e
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-02-06 10:40:39 +0200

    v-model

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ let message = ref('Hello Vue!');
 <template>
 {{ count }} <button v-on:click="count++">Click me</button><br>
 <h1>{{ message }}</h1>
-<input type="text" v-bind:value="message" v-on:input="message=$event.target.value">
+<input type="text" v-model="message">
 </template>
 
 <style scoped>
