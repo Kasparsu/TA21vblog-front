181475b556a974a8e104fb40ed63c65ffbd10475
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-02-06 10:39:07 +0200

    shorter event handling

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,12 @@ import { ref } from 'vue';
 
 let count = ref(0);
 let message = ref('Hello Vue!');
-function changeMessage(event){
-    console.log(event.target.value);
-    message.value = event.target.value;
-}
 </script>
 
 <template>
 {{ count }} <button v-on:click="count++">Click me</button><br>
 <h1>{{ message }}</h1>
-<input type="text" v-bind:value="message" v-on:input="changeMessage">
+<input type="text" v-bind:value="message" v-on:input="message=$event.target.value">
 </template>
 
 <style scoped>
