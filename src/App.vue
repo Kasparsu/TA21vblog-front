8cb6e9a209b882e9dd5624bfc5bac822d339eb72
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-02-06 10:37:27 +0200

    v-bind and events and functions

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <script setup>
+import { ref } from 'vue';
 
+let count = ref(0);
+let message = ref('Hello Vue!');
+function changeMessage(event){
+    console.log(event.target.value);
+    message.value = event.target.value;
+}
 </script>
 
 <template>
-
+{{ count }} <button v-on:click="count++">Click me</button><br>
+<h1>{{ message }}</h1>
+<input type="text" v-bind:value="message" v-on:input="changeMessage">
 </template>
 
 <style scoped>
