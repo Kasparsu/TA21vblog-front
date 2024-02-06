85027a646146b88a16462b66b07a39aef76a85ca
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-02-06 10:47:23 +0200

    Add togglable style example

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,18 @@ import { ref } from 'vue';
 
 let count = ref(0);
 let message = ref('Hello Vue!');
+let isRed = ref(false);
 </script>
 
 <template>
-{{ count }} <button v-on:click="count++">Click me</button><br>
+{{ count }} <button @click="count++">Click me</button><br>
+<button :class="{red: isRed}" @click="isRed=!isRed">Click me</button><br>
 <h1>{{ message }}</h1>
 <input type="text" v-model="message">
 </template>
 
 <style scoped>
-
+.red {
+  background-color: red;
+}
 </style>
