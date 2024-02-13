538d5c5519f6f308d85abbd68bec719178d59861
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-02-13 10:07:28 +0200

    Add VueRouter

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,11 @@
 <script setup>
-import { ref, computed } from 'vue';
-import ItemList from './components/ItemList.vue';
 
-let i = 0;
-let items = ref([
-  {id: i++, name:'Piim', isDone: true},
-  {id: i++, name:'Sai', isDone: false},
-  {id: i++, name:'Viin', isDone: false},
-  {id: i++, name:'Krõpsud', isDone: true},
-]);
-let newItem = ref('Hello');
-
-function addItem(){
-  if(newItem.value.trim() !== ''){
-    items.value.push({id: i++, name: newItem.value.trim(), isDone: false});
-  }
-  newItem.value = '';
-}
-
-let doneItems = computed(() => {
-  return items.value.filter(item => item.isDone);
-});
-let toDoItems = computed(() => {
-  return items.value.filter(item => !item.isDone);
-});
 </script>
 
 <template>
-<input type="text" v-model="newItem" @keydown.enter="addItem">
-<button @click="addItem">Add Item</button>
-<ItemList :items="items" title="All Items"></ItemList>
-<ItemList :items="doneItems" title="Done Items"></ItemList>
-<ItemList :items="toDoItems" title="ToDo Items"></ItemList>
-
-
+  <RouterLink to="/">Home</RouterLink>
+  <RouterLink to="/about">About</RouterLink>
+  <RouterView></RouterView>
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app');
+import Home from './pages/Home.vue';
+import About from './pages/About.vue';
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/about', component: About },
+]
+
+const router = createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: createWebHistory(),
+    routes, // short for `routes: routes`
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
