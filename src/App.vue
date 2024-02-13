fa9ec2fcdf7982802fe143745ff677a81d927703
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-02-13 09:16:43 +0200

    Add itemlist example

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,59 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
+let i = 0;
+let items = ref([
+  {id: i++, name:'Piim', isDone: true},
+  {id: i++, name:'Sai', isDone: false},
+  {id: i++, name:'Viin', isDone: false},
+  {id: i++, name:'Krõpsud', isDone: true},
+]);
+let newItem = ref('Hello');
 
-let count = ref(0);
-let message = ref('Hello Vue!');
-let isRed = ref(false);
+function addItem(){
+  if(newItem.value.trim() !== ''){
+    items.value.push({id: i++, name: newItem.value.trim(), isDone: false});
+  }
+  newItem.value = '';
+}
+
+let doneItems = computed(() => {
+  return items.value.filter(item => item.isDone);
+});
+let toDoItems = computed(() => {
+  return items.value.filter(item => !item.isDone);
+});
 </script>
 
 <template>
-{{ count }} <button @click="count++">Click me</button><br>
-<button :class="{red: isRed}" @click="isRed=!isRed">Click me</button><br>
-<h1>{{ message }}</h1>
-<input type="text" v-model="message">
+<input type="text" v-model="newItem" @keydown.enter="addItem">
+<button @click="addItem">Add Item</button>
+
+<h1>All Items</h1>
+<ul>
+  <li v-for="item in items" :key="item.id">
+    {{ item.name }}
+    <input type="checkbox" v-model="item.isDone">
+  </li>
+</ul>
+
+<h1>Done Items</h1>
+<ul>
+  <li v-for="item in doneItems" :key="item.id">
+    {{ item.name }}
+    <input type="checkbox" v-model="item.isDone">
+  </li>
+</ul>
+
+<h1>ToDo Items</h1>
+<ul>
+  <li v-for="item in toDoItems" :key="item.id">
+    {{ item.name }}
+    <input type="checkbox" v-model="item.isDone">
+  </li>
+</ul>
+
 </template>
 
 <style scoped>
-.red {
-  background-color: red;
-}
+
 </style>
